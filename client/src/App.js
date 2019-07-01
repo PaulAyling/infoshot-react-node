9d3a6fd9e425fdd6e7a2931bf4b50d8a9ab19cb8
Paul AYling <jimmyyukka@gmail.com>
2019-07-01 14:45:55 +0200

    Created New repo

---
+++ client/src/App.js
@@ -0,0 +1,81 @@
+// PAUL AYLING
+//  TASKS   - GITHUB REPOSITORY IS NOT WORKING - THINK ITS DUE TO THE PROXY
+// VM4901:1 GET http://localhost:3000/api/profile/github/https://github.com/PaulAyling
+// THE SAME COMMAND WORKS FOR 5000 in POSTMAN on 5000
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
+import AddEducation from './components/profile-forms/AddEducation';
+import PrivateRoute from './components/routing/PrivateRoute';
+import Posts from './components/posts/posts';
+
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+//
+import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+//
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
+              />
+              <PrivateRoute exact path='/posts' component={Posts} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+//
+export default App;
